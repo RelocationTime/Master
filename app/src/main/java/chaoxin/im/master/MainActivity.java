1c35b102add6c0f13f32357d9785b8671162d554
peter <peter.liu@liao.com>
2016-11-23 18:19:00 +0800

    add test

--- app/src/main/java/chaoxin/im/master/MainActivity.java
+++ app/src/main/java/chaoxin/im/master/MainActivity.java
@@ -2,12 +2,24 @@ package chaoxin.im.master;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText et_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        et_name = (EditText) findViewById(R.id.et_name);
+        et_name.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(MainActivity.this, "test", Toast.LENGTH_SHORT).show();
     }
 }
